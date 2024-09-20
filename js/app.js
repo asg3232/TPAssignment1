fea3bc3c91c51926a273ba71925b731cc8ceed2c
asg3232 <gibson.andrew32@gmail.com>
2024-09-19 22:50:35 -0500

    TP ASSIGNMENT 1 VERSION 1

--- js/app.js
+++ js/app.js
@@ -1,13 +1,8 @@
 /* Code for the slider adjusting the number of pain the patient is feeling */
 let slider = document.getElementById("health")
-  let output = document.getElementById("healthValue")
-  output.innerHTML = slider.value;
-  slider.oninput = function () {output.innerHTML = this.value;};
-
-/* Code for linking to my Path of Exile character from the Settlers of Kalguur league after you submit your form */
-// document.getElementById("submitButton").onclick = function () {
-//   location.href = "hw1-thankyou.html"
-// }
+let output = document.getElementById("healthValue")
+output.innerHTML = slider.value;
+slider.oninput = function () {output.innerHTML = this.value;};
 
 /* Get the current date */
 const today = new Date();
--- js/thankYou.js
+++ js/thankYou.js
@@ -1,5 +1,4 @@
 function thankYou() {
   console.log('hello');
   location.href = "hw1-thankyou.html"
-  return true;
 }
